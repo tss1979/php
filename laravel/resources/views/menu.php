58f3d57ed77f8d95e8b6207250c7494927048bed
tash1979 <tash1979@rambler.ru>
2020-03-25 23:11:19 +0300

    before l2

--- laravel/resources/views/menu.php
+++ laravel/resources/views/menu.php
@@ -1,5 +1,5 @@
 <ul>
-    <li><a href="/laravel/laravel/public">Главная</a></li>
-    <li><a href="/laravel/laravel/public/project_info">О нашем новостном портале</a></li>
-    <li><a href="/laravel/laravel/public/news">Свежие новости</a></li>
+    <li><a href="./">Главная</a></li>
+    <li><a href="./project_info">О нашем новостном портале</a></li>
+    <li><a href="./news">Свежие новости</a></li>
 </ul>
